f66d5e39cb9899e1967e75a89f56076aea4497c2
predefine <predefine@yandex.ru>
2023-08-10 18:59:41 +0700

    test

--- install.sh
+++ install.sh
@@ -1,2 +1,21 @@
 #!/usr/bin/env bash
-echo "WIP!"
+
+termux_boot_scripts_path="$HOME/.termux/boot"
+[ -d "$termux_boot_scripts_path" ] || mkdir $termux_boot_scripts_path -p
+termux_config_path="$HOME/.config"
+[ -d "$termux_config_path" ] || mkdir $termux_config_path -p
+termux_init_services_path="$HOME/.termux_init/services"
+[ -d "$termux_init_services_path" ] || mkdir $termux_init_services_path -p
+
+for i in $(find termux_boot -type f); do
+    cp $i $termux_boot_scripts_path
+    chmod +x $termux_boot_scripts_path/$(basename $i)
+done
+
+for i in $(find config -type f); do
+    cp $i $termux_config_path
+done
+
+for i in $(find services -type f); do
+    cp $i $termux_init_services_path
+done
